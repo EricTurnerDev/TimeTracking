89ebe6fc50dd35d0548535aac3b580427565c5ba
Eric Turner <eric@ericturner.dev>
2022-08-27 17:42:21 -0400

    Give the client cards some style

--- renderer/components/clientCard.tsx
+++ renderer/components/clientCard.tsx
@@ -25,8 +25,13 @@ export default function ClientCard({id, name, onClientDeleted, className}: IClie
     }
 
     return (
-        <div className={classNames('client p-2 sm:p-3 md:p-4 lg:p-5 xl:p-6 bg-blue-500 rounded', className)}>
-            <p>{name} <FontAwesomeIcon icon={faTrash} onClick={trashIconClicked}/></p>
+        <div className={classNames('client-card flex flex-col', className)}>
+            <h3 className="flex-grow p-2 sm:p-3 md:p-4 lg:p-5 xl:p-6 2xl:p-7 font-medium bg-blue-500">
+                {name}
+            </h3>
+            <div className="min-h-fit p-2 sm:p-3 md:p-4 lg:p-5 xl:p-6 2xl:p-7 bg-gray-50 text-black">
+                <FontAwesomeIcon icon={faTrash} className="hover:cursor-pointer" onClick={trashIconClicked}/>
+            </div>
         </div>
     )
 }
--- renderer/components/grid.tsx
+++ renderer/components/grid.tsx
@@ -1,6 +1,6 @@
 export default function Grid({children}) {
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-8 2xl:grid-cols-12 gap-2.5 sm:gap-3 md:gap-4 lg:gap-5 xl:gap-6 2xl:gap-7 auto-rows-fr">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-8 gap-2.5 sm:gap-3 md:gap-4 lg:gap-5 xl:gap-6 2xl:gap-7 auto-rows-fr">
             {children}
         </div>
     )
